c271c9a23873642a9b3eb4b3b5a4bf0cecb9c4ed
Fernanda Cánepa <mfercr@hotmail.com>
2018-08-17 15:10:08 -0300

    API (o no)

--- js/coverdynamicload.js
+++ js/coverdynamicload.js
@@ -1,27 +1,35 @@
-
-var imagesArray = [ "img/destacados/1.png",
-                    "img/destacados/2.png",
-                    "img/destacados/3.png",
-                    "img/destacados/4.png",
-                    "img/destacados/5.png",
-                    "img/destacados/6.png",
-                    "img/destacados/7.jpg",
-                    "img/destacados/8.jpg",
-                    "img/destacados/9.png" ]
+//
+// var imagesArray = [ "img/destacados/1.png",
+//                     "img/destacados/2.png",
+//                     "img/destacados/3.png",
+//                     "img/destacados/4.png",
+//                     "img/destacados/5.png",
+//                     "img/destacados/6.png",
+//                     "img/destacados/7.jpg",
+//                     "img/destacados/8.jpg",
+//                     "img/destacados/9.png" ]
 
 $(document).ready(function(){
 
   var container = document.getElementById('recommendedbox');
 	var docFrag = document.createDocumentFragment();
 
-  imagesArray.forEach(function(url, index, originalArray) {
+
+  fetch('http://localhost:3000/properties?isHighlight=true')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    myJson.forEach(function(element) {
       var img = document.createElement('img');
-      // $(img).addClass("");
       var theDiv = document.createElement('div');
       $(theDiv).addClass("recommendedproperty");
       $(theDiv).prepend(img);
-      img.src = url;
+      img.src = element.images;
       docFrag.appendChild(theDiv);
+  });
+
+
   });
 
   container.appendChild(docFrag);
